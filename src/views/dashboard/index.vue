e5d1bcc315aeef93be8f39c1835e52bf2d3f435c
xhn <930957853@qq.com>
2022-03-03 14:37:11 +0800

    图表显示

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
-
+    <div>
+      <div :id="id" :class="className" :style="{height:height,width:width}"/>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+import * as echarts from 'echarts';
 export default {
   name: 'Dashboard',
   computed: {
@@ -15,10 +17,154 @@ export default {
       'name'
     ])
   },
-  data() {
-    return {}
+  props: {
+    className: {
+      type: String,
+      default: 'chart'
+    },
+    id: {
+      type: String,
+      default: 'chart'
+    },
+    width: {
+      type: String,
+      default: '100%'
+    },
+    height: {
+      type: String,
+      default: '500px'
+    }
   },
-  method: {}
+  data() {
+    return {
+      chart: null
+    }
+  },
+  mounted() {
+    this.initChart()
+  },
+  beforeDestroy() {
+    if (!this.chart) {
+      return
+    }
+    this.chart.dispose()
+    this.chart = null
+  },
+  methods: {
+    initChart() {
+      this.chart = echarts.init(document.getElementById(this.id))
+      this.chart.setOption({
+        backgroundColor: '#fdfdfd',
+        title: {
+          top: 20,
+          text: '注册统计',
+          textStyle: {
+            fontWeight: 'normal',
+            fontSize: 16,
+            color: '#217d61'
+          },
+          left: '1%'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            lineStyle: {
+              color: '#76beff'
+            }
+          }
+        },
+        legend: {
+          top: 20,
+          icon: 'rect',
+          itemWidth: 14,
+          itemHeight: 5,
+          itemGap: 13,
+          data: ['注册人数'],
+          right: '4%',
+          textStyle: {
+            fontSize: 12,
+            color: '#8c8cfc'
+          }
+        },
+        grid: {
+          top: 100,
+          left: '2%',
+          right: '2%',
+          bottom: '2%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          boundaryGap: false,
+          axisLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          data: ['2-1', '2-2', '2-3', '2-4', '2-5', '2-6', '2-7','2-1', '2-2', '2-3', '2-4', '2-5', '2-6', '2-7']
+        }],
+        yAxis: [{
+          type: 'value',
+          name: '',
+          axisTick: {
+            show: false
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          axisLabel: {
+            margin: 10,
+            textStyle: {
+              fontSize: 14
+            }
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          }
+        }],
+        series: [
+          {
+            name: '注册人数',
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              normal: {
+                width: 1
+              }
+            },
+            areaStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0,
+                  color: 'rgba(82,255,212,0.3)'
+                }, {
+                  offset: 0.8,
+                  color: 'rgba(137, 189, 27, 0)'
+                }], false),
+                shadowColor: 'rgba(0, 0, 0, 0.1)',
+                shadowBlur: 10
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: 'rgb(32,255,173)',
+                borderColor: 'rgba(137,189,2,0.27)',
+                borderWidth: 12
+
+              }
+            },
+            data: [220, 182, 191, 134, 150, 120, 110, 125, 145, 122, 165, 122]
+          }]
+      })
+    }
+  }
 }
 </script>
 
